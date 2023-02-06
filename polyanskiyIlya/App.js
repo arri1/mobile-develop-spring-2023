a3c3f91c3de3774e36567b9b19552b3eab9d4028
Полянский Илья Игоревич <polyanskiy_ii@albank.ru>
2023-02-06 18:01:24 +0900

    Update field

--- polyanskiyIlya/App.js
+++ polyanskiyIlya/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Polyanskiy test</Text>
+      <Text>For merge request</Text>
       <StatusBar style="auto" />
     </View>
   );
