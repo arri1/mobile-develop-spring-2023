cf21171346a2e6bca840603b87085c1d5b0bedf1
Dusha628 <dushaborisov766@yandex.ru>
2023-02-06 19:33:29 +0900

    Initial commit

---
+++ borisovAndrey/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>I am the storm that is approuching!!! Provooooooookin! Black clouds in isolation! I am reclaimer of my nameeeeeeee!!! Born in flames! I have been blessed! My family crest is a demon of death!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
