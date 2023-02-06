bca45f851493ad37f438efee3496e000a8e6ed6f
serpentim <54822621+serpentim@users.noreply.github.com>
2023-02-06 18:59:27 +0900

    2nd commit

--- ivanovTim/App.js
+++ ivanovTim/App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-export default function App() {
+const colors = ['red', 'white', 'blue', 'black'];
+const App = () => {
+  const [count,setCount] = useState(0);
+  const [backgroundColor, setBackgroundColor] = useState(0);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView
+      style={{
+        backgroundColor,
+        flex: 1,
+        justifyContent: 'center',
+      }}
+    >
+      <View style={{ margin: 15}}>
+        <View style={{alignItems: "center"}}>
+          <Text style={{color: "white", fontSize: "20", fontWeight: "800"}}>
+            {count}
+          </Text>
+        </View>
+        <TouchableOpacity
+          onPress={() => {
+            setCount(count + 1);
+          }}
+          style={{ 
+            marginTop: 10,
+            borderRadius: 10, 
+            height: 40, 
+            backgroundColor: "blue" 
+          }}
+        />
+        <TouchableOpacity
+          onPress={() => {
+            console.log((backgroundColorIndex + 1) % ())
+          }}
+          style={{ 
+            marginTop: 10,
+            borderRadius: 10, 
+            height: 40, 
+            backgroundColor: "blue" 
+          }}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
