ab8913258b8f4d20c78cb79f7edcea250d229ac6
danildiodorov <ddldiodorov@gmail.com>
2023-02-06 18:20:38 +0900

    hello world

--- diodorovdanil/App.js
+++ diodorovdanil/App.js
@@ -4,8 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Polyanskiy test</Text>
-      <StatusBar style="auto" />
+      Hello, world!
     </View>
   );
 }
