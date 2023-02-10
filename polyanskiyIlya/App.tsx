add3bc903761d7cb32ef27b32a6c81e6badd9b44
Полянский Илья Игоревич <polyanskiy_ii@albank.ru>
2023-02-10 23:42:24 +0900

    one more initial commit

---
+++ polyanskiyIlya/App.tsx
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {StatusBar, StyleSheet, Text, View} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <View style={styles.container}>
+      <Text>Created from scratch on typescript!</Text>
+      <StatusBar />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
