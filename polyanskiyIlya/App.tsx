b307f47d1a3fbe80df0c5c30fec24dd0925d4e2e
Полянский Илья Игоревич <polyanskiy_ii@albank.ru>
2023-02-12 14:03:51 +0900

    Add navigation container to app

--- polyanskiyIlya/App.tsx
+++ polyanskiyIlya/App.tsx
@@ -1,22 +1,8 @@
 import React from 'react';
-import {StatusBar, StyleSheet, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
 
 function App(): JSX.Element {
-  return (
-    <View style={styles.container}>
-      <Text>Created from scratch on typescript!</Text>
-      <StatusBar />
-    </View>
-  );
+  return <NavigationContainer>{}</NavigationContainer>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default App;
