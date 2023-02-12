c3bf44fd60310055302da3c902f9f95fe5699706
Полянский Илья Игоревич <polyanskiy_ii@albank.ru>
2023-02-12 14:33:21 +0900

    Add lab1 view

---
+++ polyanskiyIlya/src/views/lab1/lab1.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export function Lab1({}): JSX.Element {
+  return (
+    <View>
+      <Text>Lab1 dummy</Text>
+    </View>
+  );
+}
