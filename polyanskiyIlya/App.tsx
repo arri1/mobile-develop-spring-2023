d838023a61f22b76fabdf6358ccd9303d025eb1e
Полянский Илья Игоревич <polyanskiy_ii@albank.ru>
2023-02-12 14:13:47 +0900

    `Hello world` navigation app

--- polyanskiyIlya/App.tsx
+++ polyanskiyIlya/App.tsx
@@ -1,8 +1,37 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {View, Text} from 'react-native';
+
+function FirstScreen(): JSX.Element {
+  return (
+    // eslint-disable-next-line react-native/no-inline-styles
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>This is the first screen</Text>
+    </View>
+  );
+}
+
+function SecondScreen(): JSX.Element {
+  return (
+    // eslint-disable-next-line react-native/no-inline-styles
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>This is the second screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
 
 function App(): JSX.Element {
-  return <NavigationContainer>{}</NavigationContainer>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Second">
+        <Stack.Screen name="First" component={FirstScreen} />
+        <Stack.Screen name="Second" component={SecondScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default App;
