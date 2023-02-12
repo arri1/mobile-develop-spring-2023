df420301bbb346605c051006407dc73fd0c32440
Полянский Илья Игоревич <polyanskiy_ii@albank.ru>
2023-02-12 14:33:04 +0900

    Update eslint config

--- polyanskiyIlya/.eslintrc.js
+++ polyanskiyIlya/.eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: ['@react-native-community', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
 };
