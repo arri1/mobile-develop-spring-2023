cd83127275564be81c07df2119906767dfbfa7c4
Полянский Илья Игоревич <polyanskiy_ii@albank.ru>
2023-02-12 23:39:33 +0900

    Update lab1, add todo adding and removing

--- polyanskiyIlya/src/views/lab1/lab1.tsx
+++ polyanskiyIlya/src/views/lab1/lab1.tsx
@@ -1,10 +1,69 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {Button, StyleSheet, TextInput, View} from 'react-native';
+import TodoItem, {TodoItemModel} from '../../components/TodoItem';
 
-export function Lab1({}): JSX.Element {
+const Lab1 = () => {
+  const removeTodoItem = (id: number) => {
+    setTodoList(() => {
+      return [...todoList.filter((todo: TodoItemModel) => todo.id !== id)];
+    });
+  };
+
+  const [todoList, setTodoList] = useState<TodoItemModel[]>([
+    {
+      id: 0,
+      title: 'hello world',
+    },
+  ]);
+  const [todoTextInput, setTodoInputText] = useState('');
+
   return (
-    <View>
-      <Text>Lab1 dummy</Text>
-    </View>
+    <SafeAreaView>
+      <TextInput
+        editable
+        placeholder="Задача"
+        value={todoTextInput}
+        onChangeText={setTodoInputText}
+      />
+      <View>
+        {todoList.map(item => (
+          <TodoItem key={item.id} {...item} deleteHandler={removeTodoItem} />
+        ))}
+      </View>
+      <View style={styles.btnContainer}>
+        <Button
+          title="Добавить задачу"
+          onPress={() => {
+            if (todoTextInput.length === 0) {
+              return;
+            }
+
+            setTodoList(prev => {
+              let newTodoId = Math.max(...prev.map(todo => todo.id), -1) + 1;
+
+              return [
+                ...prev,
+                {
+                  id: newTodoId,
+                  title: todoTextInput,
+                },
+              ];
+            });
+            setTodoInputText('');
+          }}
+        />
+      </View>
+    </SafeAreaView>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  btnContainer: {
+    paddingTop: 24,
+    paddingLeft: 12,
+    paddingRight: 12,
+  },
+});
+
+export default Lab1;
