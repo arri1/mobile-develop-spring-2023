c31fdfea3dbad0716924718704084343ef4f010b
ganyavasiliev <54825242+ganyavasiliev@users.noreply.github.com>
2023-02-13 06:28:01 +0900

    add выборка да или нет

--- vasilievGavril/App.js
+++ vasilievGavril/App.js
@@ -1,20 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { 
+  StyleSheet, 
+  Button,
+  Text, 
+  SafeAreaView,
+  View, 
+  Image,
+  Alert,
+} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World!!</Text>
-      <StatusBar style="auto" />
+const Separator = () => <View style={styles.separator} />;
+
+const App = () => (
+  <SafeAreaView style={styles.container}>
+    <View>
+      <Text style={styles.welcome}>
+        Это кошка?
+        </Text>
+      <Image style={styles.image_cat} source={require('./assets/cat.png')} 
+      />
+
     </View>
-  );
-}
+
+    <Separator />
+    <View style={styles.fixToText}>
+    <Button
+      title="да"
+      onPress={() => Alert.alert('правильно, это кошка)')}
+    />
+    <Button
+      title="нет"
+      onPress={() => Alert.alert('неправильно, лох)')}
+    />
+    </View>
+    </SafeAreaView>
+    
+);
+
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#F5FCFF',
+      
   },
+  welcome: {
+      fontSize: 20,
+      textAlign: 'center',
+      margin: 10,
+      
+  },
+  image_cat:{
+
+    alignItems: 'center',
+    width: 160,
+    height: 100,
+  },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  
 });
+
+export default App;
