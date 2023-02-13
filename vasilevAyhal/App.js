bae2f8b7b7de09b59c99d2b4e20470b9c9493284
Ayhal <54825050+hopeurfnis12@users.noreply.github.com>
2023-02-13 18:12:38 +0900

    add some functional

--- vasilevAyhal/App.js
+++ vasilevAyhal/App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import Home from './components/Home';
+import Details from './components/Details';
+import { View, SafeAreaView } from 'react-native';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import Like from './assets/like';
+
+const Tab = createBottomTabNavigator();
+
+const App = () => {
+    const [count, setCount] = useState(0)
+
+    const addBarBadge = () => {
+        setCount(count+1)
+    }
+    const removeBarBadge = () => {
+        if(count != 0) setCount(count-1)
+    }
+
+
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            <NavigationContainer>
+                <Tab.Navigator
+                    screenOptions={{
+                        tabBarStyle: { height: '8%' },
+                        tabBarItemStyle: { flexDirection: 'column', justifyContent: 'center' },
+                        tabBarIcon: Like,
+                        tabBarIconStyle: { maxHeight: '50%'},
+                        tabBarBadgeStyle: { maxWidth: 40, height: 'auto', alignItems: 'center', minWidth: 'auto', top: 0 },
+                        tabBarLabelStyle: { margin: 0, fontSize: 20, width: '50%' },
+                    }}
+                >
+                    <Tab.Screen name="Home" children={() => <Home addBarBadge={addBarBadge}/>} options={count != 0 ? { tabBarBadge: count } : ''}/>
+                    <Tab.Screen name="Settings" children={() => <Details removeBarBadge={removeBarBadge}/>} />
+                </Tab.Navigator>
+            </NavigationContainer>
+        </SafeAreaView>
+    );
+};
+
+export default App
