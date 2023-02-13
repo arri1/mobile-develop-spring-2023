900ff9f598d126f45f4180f9a6a73abd4b78f0d6
Полянский Илья Игоревич <polyanskiy_ii@albank.ru>
2023-02-13 14:50:57 +0900

    Use vector icon and tab navigator

--- polyanskiyIlya/App.tsx
+++ polyanskiyIlya/App.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Lab1 from './src/views/lab1/Lab1';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/AntDesign';
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="FirstLab">
-        <Stack.Screen name="FirstLab" component={Lab1} />
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="FirstLab"
+          component={Lab1}
+          options={{
+            // eslint-disable-next-line react/no-unstable-nested-components
+            tabBarIcon: ({color, size}) => (
+              <Icon name="profile" color={color} size={size} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
