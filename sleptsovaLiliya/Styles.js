053cf5af499bf9d478a30730d41d3e02f951169a
liliya <54973374+liliyasleptsova@users.noreply.github.com>
2023-02-13 17:26:06 +0900

    asd

---
+++ sleptsovaLiliya/Styles.js
@@ -0,0 +1,84 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    main: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '90%'
+    },
+    buttonRed: {
+      height: 60,
+      width: '40%',
+      backgroundColor: 'red',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    buttonOrange: {
+      height: 60,
+      width: '40%',
+      backgroundColor: 'orange',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    buttonYellow: {
+      height: 60,
+      width: '40%',
+      backgroundColor: 'yellow',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginTop: 30
+    },
+    buttonGreen: {
+        height: 60,
+        width: '40%',
+        backgroundColor: 'green',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 30
+      },
+      buttonSkyblue: {
+        height: 60,
+        width: '40%',
+        backgroundColor: '#7FFFD4',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 30
+      },
+      buttonBlue: {
+        height: 60,
+        width: '40%',
+        backgroundColor: 'blue',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 30
+      },
+      buttonPurple: {
+        height: 60,
+        width: '90%',
+        backgroundColor: 'purple',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 0
+      },
+    buttonText: {
+      color: 'white',
+      fontSize: 20
+    },
+    container: {
+      width: '80%',
+      display: 'flex',
+      alignItems: 'center',
+      marginTop: 70,
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      color:'black'
+    }
+  })
