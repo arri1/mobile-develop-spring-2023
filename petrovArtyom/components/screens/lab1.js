31b4348c4cda72e536226c6889c309f35722cc0f
perestail228 <artemperespetrov3@gmail.com>
2023-02-24 22:22:00 +0900

    add_navigation
    was added navigation

---
+++ petrovArtyom/components/screens/lab1.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { SafeAreaView, Text, TouchableOpacity, View } from "react-native";
+
+const colors = ["red", "white", "blue", "black"];
+
+const Lab1 = () => {
+  const [count, setCount] = useState(0);
+  const [backgroundColorIndex, setBackgroundColorIndex] = useState(0);
+
+  return (
+    <SafeAreaView
+      style={{
+        backgroundColor: colors[backgroundColorIndex],
+        flex: 1,
+        justifyContent: "center",
+      }}
+    >
+      <View style={{ margin: 15 }}>
+        <View style={{ alignItems: "center" }}>
+          <Text style={{ color: "white", fontSize: 20, fontWeight: "800" }}>
+            {count}
+          </Text>
+        </View>
+        <TouchableOpacity
+          onPress={() => {
+            setCount(count + 1);
+          }}
+          style={{
+            marginTop: 10,
+            borderRadius: 10,
+            height: 40,
+            backgroundColor: colors[(backgroundColorIndex + 1) % colors.length],
+          }}
+        />
+        <TouchableOpacity
+          onPress={() => {
+            setBackgroundColorIndex((backgroundColorIndex + 1) % colors.length);
+          }}
+          style={{
+            marginTop: 10,
+            borderRadius: 10,
+            height: 40,
+            backgroundColor: colors[(backgroundColorIndex + 1) % colors.length],
+          }}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Lab1;
---
+++ petrovArtyom/components/screens/lab2.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const Lab2 = () => {
+  return (
+    <View>
+      <Text>Lab2</Text>
+    </View>
+  );
+};
+
+export default Lab2;
