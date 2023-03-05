45030af126220d2adf99c74361676f495084b533
Sanaaia <okhlopkova.sanaaia@gmail.com>
2023-03-06 05:21:19 +0900

    lab3 add timer with useMemo

--- okhlopkovaSanaaia/components/screens/lab3.js
+++ okhlopkovaSanaaia/components/screens/lab3.js
@@ -1,10 +1,38 @@
 import { StyleSheet, SafeAreaView, View, Text, Button } from "react-native";
 import DateTimePickerModal from "react-native-modal-datetime-picker";
-import { useState } from "react";
+import { useState, useEffect, useMemo } from "react";
 
 const Lab3 = () => {
-  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [selectedDate, setSelectedDate] = useState(
+    new Date("2024-01-01T00:00:00")
+  );
   const [datePickerVisible, setDatePickerVisible] = useState(false);
+
+  const parsedDate = useMemo(() => {
+    return Date.parse(selectedDate);
+  }, [selectedDate]);
+
+  const [time, setTime] = useState(parsedDate - Date.now());
+  const [over, setOver] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const difference = parsedDate - Date.now();
+      if (difference > 0) {
+        setTime(difference);
+      } else {
+        setOver(true);
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [parsedDate]);
+
+  const timeData = {
+    Days: Math.floor(time / (1000 * 60 * 60 * 24)),
+    Hours: Math.floor((time / (1000 * 60 * 60)) % 24),
+    Minutes: Math.floor((time / 1000 / 60) % 60),
+    Seconds: Math.floor((time / 1000) % 60),
+  };
 
   const showDatePicker = () => {
     setDatePickerVisible(true);
@@ -22,7 +50,17 @@ const Lab3 = () => {
 
   return (
     <SafeAreaView style={styles.main}>
-      <Text style={styles.text}>{selectedDate.toLocaleString()}</Text>
+      <View style={styles.container}>
+        {Object.entries(timeData).map(([label, value]) => (
+          <View key={label} style={styles.box}>
+            <Text style={styles.time}>{`${Math.floor(value)}`}</Text>
+            <Text style={styles.time}>{label}</Text>
+          </View>
+        ))}
+      </View>
+      <Text style={styles.text}>
+        Left until {selectedDate.toLocaleString()}
+      </Text>
       <View style={styles.text}>
         <Button title="Change the date" onPress={showDatePicker} />
       </View>
@@ -37,6 +75,9 @@ const Lab3 = () => {
         locale="ru-RU"
         pickerStyleIOS={styles.picker}
       />
+      <Text style={[styles.text, { color: "red" }]}>
+        {over ? "Time's up!" : ""}
+      </Text>
     </SafeAreaView>
   );
 };
@@ -49,10 +90,28 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 18,
-    marginBottom: 20,
+    marginBottom: 15,
   },
   picker: {
     marginHorizontal: 15,
+  },
+  container: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    marginVertical: 30,
+  },
+  box: {
+    padding: 10,
+    borderRadius: 10,
+    marginHorizontal: 5,
+    minWidth: "20%",
+    backgroundColor: "white",
+    alignSelf: "center",
+    justifyContent: "space-evenly",
+  },
+  time: {
+    textAlign: "center",
+    fontSize: 16,
   },
 });
 
