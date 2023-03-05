acd869944eeda574187d6bcd77b4a03887b2e655
Sanaaia <okhlopkova.sanaaia@gmail.com>
2023-03-06 05:19:02 +0900

    lab3 add date time picker

--- okhlopkovaSanaaia/components/screens/lab3.js
+++ okhlopkovaSanaaia/components/screens/lab3.js
@@ -1,19 +1,58 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, SafeAreaView, View, Text, Button } from "react-native";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+import { useState } from "react";
 
 const Lab3 = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [datePickerVisible, setDatePickerVisible] = useState(false);
+
+  const showDatePicker = () => {
+    setDatePickerVisible(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisible(false);
+  };
+
+  const handleConfirm = (date) => {
+    setSelectedDate(date);
+    setOver(false);
+    hideDatePicker();
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Hello from Sanaaia!</Text>
-    </View>
+    <SafeAreaView style={styles.main}>
+      <Text style={styles.text}>{selectedDate.toLocaleString()}</Text>
+      <View style={styles.text}>
+        <Button title="Change the date" onPress={showDatePicker} />
+      </View>
+      <DateTimePickerModal
+        date={selectedDate}
+        isVisible={datePickerVisible}
+        mode="datetime"
+        display={Platform.OS === "ios" ? "inline" : "default"}
+        onConfirm={handleConfirm}
+        onCancel={hideDatePicker}
+        minimumDate={new Date()}
+        locale="ru-RU"
+        pickerStyleIOS={styles.picker}
+      />
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  main: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  text: {
+    fontSize: 18,
+    marginBottom: 20,
+  },
+  picker: {
+    marginHorizontal: 15,
   },
 });
 
