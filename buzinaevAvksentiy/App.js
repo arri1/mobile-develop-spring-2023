4d5153be0e594b27923061f0b32cd8808c8663d2
Buzinaev <7Nya95@mail.ru>
2023-03-13 18:55:21 +0900

    lab1

---
+++ buzinaevAvksentiy/App.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, SafeAreaView } from "react-native";
+import Lab1 from "./components/lab1";
+
+
+const App = () => {
+  return <Lab1/>
+}; 
+
+
+export default App;
