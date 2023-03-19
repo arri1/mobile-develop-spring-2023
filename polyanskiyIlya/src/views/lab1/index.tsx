df035e9ebbbca1746e13617ba782af4703ecae5e
Полянский Илья Игоревич <polyanskiy_ii@albank.ru>
2023-03-19 22:04:53 +0900

    Update Lab1 component using custom components

--- polyanskiyIlya/src/views/lab1/index.tsx
+++ polyanskiyIlya/src/views/lab1/index.tsx
@@ -1,69 +1,57 @@
 import React, {useState} from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import {Button, StyleSheet, TextInput, View} from 'react-native';
-import TodoItem, {TodoItemModel} from '../../components/TodoItem';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import {CellModel} from '../../components/ListCell';
+import ListView from '../../components/ListView';
 
 const Lab1 = () => {
-  const [todoList, setTodoList] = useState<TodoItemModel[]>([
+  const [todoList, setTodoList] = useState<CellModel[]>([
     {
       id: 0,
-      title: 'hello world',
+      title:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras quis fermentum www',
     },
   ]);
   const [todoTextInput, setTodoInputText] = useState('');
 
   const removeTodoItem = (id: number) => {
     setTodoList(() => {
-      return [...todoList.filter((todo: TodoItemModel) => todo.id !== id)];
+      return [...todoList.filter((todo: CellModel) => todo.id !== id)];
     });
   };
 
   return (
     <SafeAreaView>
-      <TextInput
-        editable
-        placeholder="Задача"
+      <Input
+        placeholder="Текст задачи"
         value={todoTextInput}
-        onChangeText={setTodoInputText}
+        onChange={setTodoInputText}
       />
-      <View>
-        {todoList.map(item => (
-          <TodoItem key={item.id} {...item} deleteHandler={removeTodoItem} />
-        ))}
-      </View>
-      <View style={styles.btnContainer}>
-        <Button
-          title="Добавить задачу"
-          onPress={() => {
-            if (todoTextInput.length === 0) {
-              return;
-            }
+      <Button
+        title="Добавить задачу"
+        onPress={() => {
+          if (todoTextInput.length === 0) {
+            return;
+          }
 
-            setTodoList(prev => {
-              const newTodoId = Math.max(...prev.map(todo => todo.id), -1) + 1;
+          setTodoList(prev => {
+            const newTodoId = Math.max(...prev.map(todo => todo.id), -1) + 1;
 
-              return [
-                ...prev,
-                {
-                  id: newTodoId,
-                  title: todoTextInput,
-                },
-              ];
-            });
-            setTodoInputText('');
-          }}
-        />
-      </View>
+            return [
+              ...prev,
+              {
+                id: newTodoId,
+                title: todoTextInput,
+              },
+            ];
+          });
+          setTodoInputText('');
+        }}
+      />
+      <ListView cellList={todoList} removeHandler={removeTodoItem} />
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  btnContainer: {
-    paddingTop: 24,
-    paddingLeft: 12,
-    paddingRight: 12,
-  },
-});
 
 export default Lab1;
