6082dbb9126f98f4cecb75bd451171153611ee3b
egor dyakonov <egordyakonov2000@gmail.com>
2023-03-20 20:24:48 +0900

    Lab2

--- dyakonovEgor/components/navigation/tabNavigation.js
+++ dyakonovEgor/components/navigation/tabNavigation.js
@@ -1,13 +1,11 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 // import Icon from "react-native-vector-icons/FontAwesome";
 import Lab2 from "../screens/lab2";
-import Lab1 from "../screens/Lab1";
 
 const Tab = createBottomTabNavigator();
 const TabNavigator = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Lab1" component={Lab1} />
       <Tab.Screen name="Lab2" component={Lab2} />
     </Tab.Navigator>
   );
