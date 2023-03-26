384595526eb4f013bf87d27dceed7dd37e38dea7
abbadon321 <86795437+abbadon321@users.noreply.github.com>
2023-03-26 22:17:01 +0900

    add apollo lab

--- sobyaninSergey/components/navigations/tabNavigation.js
+++ sobyaninSergey/components/navigations/tabNavigation.js
@@ -4,6 +4,7 @@ import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityI
 import Lab1 from "../screens/lab1";
 import Lab2 from "../screens/lab2";
 import Lab3 from "../screens/lab3";
+import ApolloLab from "../screens/lab Apollo";
 
 const Tab = createBottomTabNavigator();
 const TabNavigation = () => {
@@ -58,6 +59,16 @@ const TabNavigation = () => {
           tabBarLabel: "LAB 3",
           tabBarIcon: ({ color }) => (
             <MaterialCommunityIcons name="memory" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="LAB APOLLO"
+        component={ApolloLab}
+        options={{
+          tabBarLabel: "APOLLO LAB",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="movie" color={color} size={26} />
           ),
         }}
       />
