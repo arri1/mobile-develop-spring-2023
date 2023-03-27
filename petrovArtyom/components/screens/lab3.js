b910d8b73c1bf47b2b026fd4f437777cbcb08ac9
perestail228 <artemperespetrov3@gmail.com>
2023-03-27 13:26:20 +0900

    useMemo

--- petrovArtyom/components/screens/lab3.js
+++ petrovArtyom/components/screens/lab3.js
@@ -9,13 +9,13 @@ import {
 import { useState, useMemo } from "react";
 
 const data = [
-  {id: 1, state: 'Texas', rating: 4.5},
-  {id: 2, state: 'Hawaii', rating: 3},
-  {id: 3, state: 'Illinois', rating: 4},
-  {id: 4, state: 'Texas', rating: 5},
-  {id: 5, state: 'Ohio', rating: 4.5},
-  {id: 6, state: 'Louisiana', rating: 3},
-  {id: 7, state: 'Texas', rating: 2},
+  {id: 1, state: 'Молоко', rating: 4.5},
+  {id: 2, state: 'Сыр', rating: 3},
+  {id: 3, state: 'Чеснок', rating: 4},
+  {id: 4, state: 'Броколи', rating: 5},
+  {id: 5, state: 'Шаурма', rating: 4.5},
+  {id: 6, state: 'Хлеб', rating: 3},
+  {id: 7, state: 'Колбаса', rating: 2},
   // ...
   // {id: 1000, state: 'asd', rating: 2},
 ]
