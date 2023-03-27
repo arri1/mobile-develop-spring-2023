88b8cf1a087b03d781389ea59857fe1217e8fc0b
perestail228 <artemperespetrov3@gmail.com>
2023-03-27 11:05:31 +0900

    useMemo

--- petrovArtyom/components/screens/lab3.js
+++ petrovArtyom/components/screens/lab3.js
@@ -1,7 +1,48 @@
-import { View } from "react-native";
+import {
+  View,
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  Button,
+  FlatList,
+} from "react-native";
+import { useState, useMemo } from "react";
+
+const data = [
+  {id: 1, state: 'Texas', rating: 4.5},
+  {id: 2, state: 'Hawaii', rating: 3},
+  {id: 3, state: 'Illinois', rating: 4},
+  {id: 4, state: 'Texas', rating: 5},
+  {id: 5, state: 'Ohio', rating: 4.5},
+  {id: 6, state: 'Louisiana', rating: 3},
+  {id: 7, state: 'Texas', rating: 2},
+  // ...
+  // {id: 1000, state: 'asd', rating: 2},
+]
+
 
 const Lab3 = () => {
-  return <View></View>;
+  const rateCompare = 3;
+
+    const computedValue = useMemo(() => {
+        const result = data.filter((d) => d.rating > rateCompare);
+        return result;
+    }, [rateCompare]);
+
+      const renderItem = ({ item }) => (
+          <View style={{padding: 10, paddingLeft: 10}}>
+            <Text>{item.state}</Text>
+          </View>
+      );
+    return (
+        <FlatList
+          data={computedValue}
+          renderItem={renderItem}
+          keyExtractor={item => item.id}
+        />
+    )
 };
+
+
 
 export default Lab3;
