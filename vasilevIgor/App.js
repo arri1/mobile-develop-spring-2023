0463e03fb42ebb5fe98cff71d6d1fc0c99ba5fe1
overfrenzy <xplo1d387@gmail.com>
2023-03-30 19:20:26 +0900

    Lab1
    useState
    couldn't do checkboxes on react-native

--- vasilevIgor/App.js
+++ vasilevIgor/App.js
@@ -1,51 +1,109 @@
-import { useState } from "react";
-import { SafeAreaView, Text, TouchableOpacity, View } from "react-native";
+import React, { useState } from "react";
+import NoteList from './NoteList';
+import {StyleSheet, View, TextInput} from "react-native";
+//import { SafeAreaView, Text, TouchableOpacity, View } from "react-native";
 
-const colors = ["red", "white", "blue", "black"];
+//const colors = ["red", "white", "blue", "black"];
 
 const App = () => {
-  const [count, setCount] = useState(0);
-  const [backgroundColorIndex, setBackgroundColorIndex] = useState(0);
+  const [notes, setNotes] = useState([
+    {id: 1, title: 'First note', relevant: true},
+    {id: 2, title: 'Second note', relevant: true},
+  ]);
+  const [noteName, setNoteName] = useState(' ');
+  const addNote = event => {
+    if (event.key === 'Enter') {
+      setNotes([
+        ...notes,
+        {
+          id: Date.now(),
+          name: noteName,
+          relevant: true
+        }
+      ])
+      setNoteName(0)
+    }
+  }
 
   return (
-    <SafeAreaView
-      style={{
-        backgroundColor: colors[backgroundColorIndex],
-        flex: 1,
-        justifyContent: "center",
-      }}
-    >
-      <View style={{ margin: 15 }}>
-        <View style={{ alignItems: "center" }}>
-          <Text style={{ color: "white", fontSize: 20, fontWeight: "800" }}>
-            {count}
-          </Text>
+    
+    // <SafeAreaView
+    //   style={{
+    //     backgroundColor: colors[backgroundColorIndex],
+    //     flex: 1,
+    //     justifyContent: "center",
+    //   }}
+    // >
+    //   <View style={{ margin: 15 }}>
+    //     <View style={{ alignItems: "center" }}>
+    //       <Text style={{ color: "white", fontSize: 20, fontWeight: "800" }}>
+    //         {count}
+    //       </Text>
+    //     </View>
+    //     <TouchableOpacity
+    //       onPress={() => {
+    //         setCount(count + 1);
+    //       }}
+    //       style={{
+    //         marginTop: 10,
+    //         borderRadius: 10,
+    //         height: 40,
+    //         backgroundColor: colors[(backgroundColorIndex + 1) % colors.length],
+    //       }}
+    //     />
+    //     <TouchableOpacity
+    //       onPress={() => {
+    //         setBackgroundColorIndex((backgroundColorIndex + 1) % colors.length);
+    //       }}
+    //       style={{
+    //         marginTop: 10,
+    //         borderRadius: 10,
+    //         height: 40,
+    //         backgroundColor: colors[(backgroundColorIndex + 1) % colors.length],
+    //       }}
+    //     />
+    //   </View>
+    // </SafeAreaView>
+    // This code was made during the class, we will not use it
+    <View style={styles.container}>
+
+      <View style={styles.form}>
+        <TextInput 
+        style={styles.input}
+        value={noteName}
+        onChange={event => setNoteName(event.target,value)}
+        onKeyDown={addNote}
+        placeholder="New note"
+        />
         </View>
-        <TouchableOpacity
-          onPress={() => {
-            setCount(count + 1);
-          }}
-          style={{
-            marginTop: 10,
-            borderRadius: 10,
-            height: 40,
-            backgroundColor: colors[(backgroundColorIndex + 1) % colors.length],
-          }}
-        />
-        <TouchableOpacity
-          onPress={() => {
-            setBackgroundColorIndex((backgroundColorIndex + 1) % colors.length);
-          }}
-          style={{
-            marginTop: 10,
-            borderRadius: 10,
-            height: 40,
-            backgroundColor: colors[(backgroundColorIndex + 1) % colors.length],
-          }}
-        />
-      </View>
-    </SafeAreaView>
+
+    <NoteList notes={notes} />
+    </View>
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f0f3f7",
+  },
+  input: {
+    backgroundColor: "white",
+    margin: 20,
+    padding: 10,
+    borderColor: "black",
+    width: "90%",
+    height: 50,
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: "silver",
+    borderColor: "#838FFF",
+  },
+  form: {
+    flex: 1,
+    alignItems: "center",
+    marginBottom: 40,
+  },
+});
---
+++ vasilevIgor/NoteList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import NoteItem from './NoteItem';
+import {StyleSheet, ScrollView, Dimensions} from "react-native";
+
+const screenWidth = Dimensions.get("window").width;
+
+const NoteList = ({notes}) => {
+    return (
+        <ScrollView contentContainerStyle={styles.scrollView}>
+            {notes.map(item => <NoteItem key={item.id} {...item} />)}
+        </ScrollView>
+    )
+}
+
+export default NoteList;
+
+const styles = StyleSheet.create({
+    scrollView: {
+        width: screenWidth,
+        alignItems: "center",
+      }
+});
