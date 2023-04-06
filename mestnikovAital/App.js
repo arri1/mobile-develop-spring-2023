1d15576f93d699742a1e7b74fdd249396043f076
Aital <90888185+latiAM@users.noreply.github.com>
2023-04-06 20:05:33 +0900

    lab1 with useState & lab2 useEffect

--- mestnikovAital/App.js
+++ mestnikovAital/App.js
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Lab1 from "./components/Lab1";
+import Lab2 from "./components/Lab2";
+import Lab3 from "./components/Lab3";
+import Ionicons from "react-native-vector-icons/Ionicons";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Labs"
+        screenOptions={({ route }) => ({
+          tabBarActiveTintColor: "#00ff00",
+          tabBarInactiveTintColor: "#483D8B",
+        })}
+      >
+        <Tab.Screen
+          name="Get image"
+          component={Lab1}
+          options={{
+            tabBarLabel: "Lab1",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="ios-image" color={color} size={size} />
+            ),
+            tabBarBadge: 1,
+          }}
+        />
+        <Tab.Screen
+          name="Lab2"
+          component={Lab2}
+          options={{
+            tabBarLabel: "Lab2",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="ios-moon" color={color} size={size} />
+            ),
+            tabBarBadge: 1,
+          }}
+        />
+        <Tab.Screen
+          name="Lab3"
+          component={Lab3}
+          options={{
+            tabBarLabel: "Lab3",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="ios-book" color={color} size={size} />
+            ),
+            tabBarBadge: 1,
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
