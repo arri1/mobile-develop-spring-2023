1570b72ffe779dab9a9b761e811ab0132c0dfbb0
Aital <90888185+latiAM@users.noreply.github.com>
2023-04-06 18:51:32 +0900

    new folder

--- mestnikovAital/App.js
+++ mestnikovAital/App.js
@@ -1,58 +1,20 @@
-import { useState, useEffect } from "react";
-import {
-  Image,
-  ImageBackground,
-  StyleSheet,
-  SafeAreaView,
-  Text,
-  Button,
-  Alert,
-  TouchableOpacity,
-  TouchableHighlight,
-  View,
-} from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-import Lab1 from "./components/Lab1.js";
-import Lab2 from "./components/Lab2.js";
-import Lab3 from "./components/Lab3.js";
-
-const img = require("./images/moon.png");
-
-const Tab = createBottomTabNavigator();
-const App = () => {
-  const [currentDate, setCurrentDate] = useState("");
-  useEffect(() => {
-    const date = new Date().getDate(); //Current Date
-    const month = new Date().getMonth() + 1; //Current Month
-    const year = new Date().getFullYear(); //Current Year
-    setCurrentDate(date + "." + month + "." + year);
-  }, []);
-
+export default function App() {
   return (
-    <SafeAreaView>
-      <View style={{ marginTop: 45 }}>
-        <View style={{ alignItems: "center" }}>
-          <Text style={{ color: "purple", fontSize: 14, fontWeight: "800" }}>
-            {currentDate}
-          </Text>
-        </View>
-      </View>
-      <NavigationContainer>
-        <Tab.Navigator
-          screenOptions={{ headerShown: false }}
-          activeColor="#f0edf6"
-          inactiveColor="#3e2465"
-          barStyle={{ backgroundColor: "#694fad" }}
-        >
-          <Tab.Screen name="LAB 1" component={Lab1} />
-          <Tab.Screen name="LAB 2" component={Lab2} />
-          <Tab.Screen name="LAB 3" component={Lab3} />
-        </Tab.Navigator>
-      </NavigationContainer>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
-};
+}
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
