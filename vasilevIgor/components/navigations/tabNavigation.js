b016e91b8bce167fdc31b0dede8883e50fd787d4
overfrenzy <xplo1d387@gmail.com>
2023-04-18 01:39:46 +0900

    Vasilev Igor Lab2+3
    fixed lab1 into more simple app, added simple uses of hooks on lab2+3

--- vasilevIgor/components/navigations/tabNavigation.js
+++ vasilevIgor/components/navigations/tabNavigation.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
-import { FontAwesome5 } from '@expo/vector-icons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import Lab1 from "../screens/lab1";
 import Lab2 from "../screens/lab2";
 import Lab3 from "../screens/lab3";
@@ -13,15 +13,22 @@ const TabNavigation = () => {
                 tabBarIcon: ({ focused, color, size }) => {
                     let iconName;
 
-                    if (route.name === "Lab1") {
-                        iconName = focused ? "cloud-sun" : "cloud";
-                    } else if (route.name === "Lab2") {
-                        iconName = focused ? "cloud-sun" : "cloud";
-                    } else if (route.name === "Lab3") {
-                        iconName = focused ? "cloud-rain" : "cloud";
-                    }
+                    if (route.name === 'Lab1') {
+                        iconName = focused
+                          ? 'ios-information-circle'
+                          : 'ios-information-circle-outline';
+                      } else if (route.name === 'Lab2') {
+                        iconName = focused
+                          ? 'ios-information-circle'
+                          : 'ios-information-circle-outline';
+                      }
+                        else if (route.name === 'Lab3') {
+                            iconName = focused
+                            ? 'ios-information-circle'
+                            : 'ios-information-circle-outline';
+                      }
 
-                    return <FontAwesome5 name={iconName} size={size} color={color} />;
+                      return <Ionicons  name={iconName} size={size} color={color} />;
                 },
             })}
         >
