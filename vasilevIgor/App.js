86736e623c78b3ba28e7a3521179ad6e65b5e3ed
overfrenzy <xplo1d387@gmail.com>
2023-04-19 16:51:01 +0900

    Lab6 GraphQL
    Lab6 GraphQL Apollo Client

--- vasilevIgor/App.js
+++ vasilevIgor/App.js
@@ -1,12 +1,20 @@
 import TabNavigation from "./components/navigations/tabNavigation";
 import { NavigationContainer } from "@react-navigation/native";
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import * as React from 'react';
+
+const client = new ApolloClient({
+  uri: 'https://flyby-router-demo.herokuapp.com/',
+  cache: new InMemoryCache(),
+});
 
 const App = () => {
   return (
+    <ApolloProvider client={client}>
     <NavigationContainer>
       <TabNavigation />
     </NavigationContainer>
+    </ApolloProvider>
   );
 };
 
--- vasilevIgor/components/navigations/tabNavigation.js
+++ vasilevIgor/components/navigations/tabNavigation.js
@@ -4,6 +4,7 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 import Lab1 from "../screens/lab1";
 import Lab2 from "../screens/lab2";
 import Lab3 from "../screens/lab3";
+import Lab6 from "../screens/lab6";
 
 const Tab = createBottomTabNavigator();
 const TabNavigation = () => {
@@ -27,6 +28,12 @@ const TabNavigation = () => {
                             ? 'ios-information-circle'
                             : 'ios-information-circle-outline';
                       }
+                      else if (route.name === 'Lab6') {
+                        iconName = focused
+                        ? 'ios-information-circle'
+                        : 'ios-information-circle-outline';
+                  }
+
 
                       return <Ionicons  name={iconName} size={size} color={color} />;
                 },
@@ -35,6 +42,7 @@ const TabNavigation = () => {
             <Tab.Screen name="Lab1" component={Lab1} />
             <Tab.Screen name="Lab2" component={Lab2} />
             <Tab.Screen name="Lab3" component={Lab3} />
+            <Tab.Screen name="Lab6" component={Lab6} />
         </Tab.Navigator>
     );
 };
