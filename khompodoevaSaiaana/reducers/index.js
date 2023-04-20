c3c19e38a231c22f44140f966c10ccb6e3a9c541
aomisaiki <skhompodoeva@gmail.com>
2023-04-20 10:45:21 +0900

    redux implemented to lab1

--- khompodoevaSaiaana/reducers/index.js
+++ khompodoevaSaiaana/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import imageReducer from './imageReducer.js';
+import reducer from './reducer.js'
 
 const rootReducer = combineReducers({
   image: imageReducer,
+  pokemon: reducer
 });
 
 export default rootReducer;
--- khompodoevaSaiaana/screens/lab1.js
+++ khompodoevaSaiaana/screens/lab1.js
@@ -1,36 +1,37 @@
 import { StyleSheet, Text, Image, View } from 'react-native';
-import React, { useState } from 'react';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import AppButton from '../components/appButton';
 
 const Lab1 = () => {
-    const [pokemon, setPokemon] = useState(1000);
-    const nextPokemon = () => {
-        pokemon < 1008 ? setPokemon(pokemon+1) : setPokemon(1)
-    }
-    const backPokemon = () => {
-        pokemon > 1 ? setPokemon(pokemon-1) : setPokemon(1008)
-    }
-    return (
-      <View style={styles.container}>
-        <Image
-          style={styles.image}
-          source={{
-            uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon}.png`,
+  const dispatch = useDispatch();
+  const pokemonNumber = useSelector((state) => state.pokemon.pokemonNumber);
+
+  const nextPokemon = () => {
+    dispatch({ type: 'NEXT_POKEMON' });
+  };
+
+  const backPokemon = () => {
+    dispatch({ type: 'BACK_POKEMON' });
+  };
+
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.image}
+        source={{
+          uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonNumber}.png`,
         }}
-        />
-        <Text style={{
-            fontSize: 28,
-            fontWeight: 'bold'
-            }}>Pokemon {pokemon}</Text>
-        <AppButton
-          onPress={() => backPokemon()}
-          title='Back'/>
-        <AppButton
-          onPress={() => nextPokemon()}
-          title='Next'/>
-      </View>
-    );
-  }
+      />
+      <Text style={{ fontSize: 28, fontWeight: 'bold' }}>
+        Pokemon {pokemonNumber}
+      </Text>
+      <AppButton onPress={() => backPokemon()} title="Back" />
+      <AppButton onPress={() => nextPokemon()} title="Next" />
+    </View>
+  );
+};
+
   const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -46,3 +47,55 @@ const Lab1 = () => {
 });
 
 export default Lab1;
+
+
+
+
+// import { StyleSheet, Text, Image, View } from 'react-native';
+// import React, { useState } from 'react';
+// import AppButton from '../components/appButton';
+
+// const Lab1 = () => {
+//     const [pokemon, setPokemon] = useState(1000);
+//     const nextPokemon = () => {
+//         pokemon < 1008 ? setPokemon(pokemon+1) : setPokemon(1)
+//     }
+//     const backPokemon = () => {
+//         pokemon > 1 ? setPokemon(pokemon-1) : setPokemon(1008)
+//     }
+//     return (
+//       <View style={styles.container}>
+//         <Image
+//           style={styles.image}
+//           source={{
+//             uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon}.png`,
+//         }}
+//         />
+//         <Text style={{
+//             fontSize: 28,
+//             fontWeight: 'bold'
+//             }}>Pokemon {pokemon}</Text>
+//         <AppButton
+//           onPress={() => backPokemon()}
+//           title='Back'/>
+//         <AppButton
+//           onPress={() => nextPokemon()}
+//           title='Next'/>
+//       </View>
+//     );
+//   }
+//   const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         backgroundColor: '#fff',
+//         padding: 50,
+//     },
+//     image: {
+//         alignSelf: 'center',
+//         width: 350,
+//         height: 350,
+//         resizeMode: 'contain',
+//     },
+// });
+
+// export default Lab1;
