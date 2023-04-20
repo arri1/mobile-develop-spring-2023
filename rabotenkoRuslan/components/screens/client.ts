68402d52421f2bdb1c9a4e56195d195e17971ab9
Rabotenko Ruslan Alekseevich <54973228+ruslanrb@users.noreply.github.com>
2023-04-20 19:34:34 +0900

    new function ADD BOOK

--- rabotenkoRuslan/components/screens/client.ts
+++ rabotenkoRuslan/components/screens/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://example.com/graphql',
+  uri: 'https://graphqlzero.almansi.me/api',
   cache: new InMemoryCache(),
 });
 
--- rabotenkoRuslan/components/screens/lab5.tsx
+++ rabotenkoRuslan/components/screens/lab5.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { useQuery } from '@apollo/client';
-import { View, Text } from 'react-native';
+import { useQuery, useMutation } from '@apollo/client';
+import { View, Text, TextInput, Button } from 'react-native';
 import { gql } from 'graphql-tag';
 
 const GET_BOOKS = gql`
@@ -13,8 +13,30 @@ const GET_BOOKS = gql`
   }
 `;
 
+const ADD_BOOK = gql`
+  mutation AddBook($title: String!, $author: String!) {
+    addBook(title: $title, author: $author) {
+      id
+      title
+      author
+    }
+  }
+`;
+
 const BookList = () => {
-  const { loading, error, data } = useQuery(GET_BOOKS);
+  const { loading, error, data, refetch } = useQuery(GET_BOOKS);
+  const [addBook] = useMutation(ADD_BOOK);
+
+  const [title, setTitle] = React.useState('');
+  const [author, setAuthor] = React.useState('');
+
+  const handleAddBook = () => {
+    addBook({ variables: { title, author } }).then(() => {
+      setTitle('');
+      setAuthor('');
+      refetch();
+    });
+  };
 
   if (loading) {
     return <Text>Loading...</Text>;
@@ -26,6 +48,21 @@ const BookList = () => {
 
   return (
     <View>
+      <View style={{ flexDirection: 'row' }}>
+        <TextInput
+          value={title}
+          onChangeText={setTitle}
+          placeholder="Enter book title"
+          style={{ flex: 1 }}
+        />
+        <TextInput
+          value={author}
+          onChangeText={setAuthor}
+          placeholder="Enter author name"
+          style={{ flex: 1 }}
+        />
+        <Button title="Add" onPress={handleAddBook} />
+      </View>
       {data.books.map((book: { id: string; title: string; author: string }) => (
         <View key={book.id}>
           <Text>{book.title}</Text>
