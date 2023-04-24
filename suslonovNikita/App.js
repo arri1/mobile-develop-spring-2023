e4178ba52933eab7763289c8d4debb7e06bae059
Second-Sail <nik200211@gmail.com>
2023-04-24 15:49:15 +0900

    added useEffect

--- suslonovNikita/App.js
+++ suslonovNikita/App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import UseState from './UseState';
+import UseEffect from './UseEffect';
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Text>Suslonov</Text>
       <StatusBar style="auto" />
       <UseState/>
+      <UseEffect/>
     </View>
   );
 }
