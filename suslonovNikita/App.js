f88f80b77da5de9739b28fa902503fe283be247c
Second-Sail <nik200211@gmail.com>
2023-04-24 15:39:05 +0900

    added useState

--- suslonovNikita/App.js
+++ suslonovNikita/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import UseState from './UseState';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Suslonov</Text>
       <StatusBar style="auto" />
+      <UseState/>
     </View>
   );
 }
