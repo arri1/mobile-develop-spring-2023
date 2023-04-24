4e6911afa2418f4f38e0247eaac6823cb67eba91
Second-Sail <nik200211@gmail.com>
2023-04-24 17:29:32 +0900

    added Appolo GraphQL

--- suslonovNikita/App.js
+++ suslonovNikita/App.js
@@ -3,59 +3,77 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import { Provider } from 'react-redux';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
 import store from './Redux'
 import UseState from './UseState';
 import UseEffect from './UseEffect';
 import UseMemo from './UseMemo';
 import ScreenRedux from './ScreenRedux';
+import GraphQL from './GraphQL';
+
 import Bus from './assets/svg/bus';
 import Basketball from './assets/svg/basketball';
 import Credit from './assets/svg/credit';
 import Iphone from './assets/svg/iphone';
+import Car from './assets/svg/car';
 
 const Tab = createBottomTabNavigator();
+// Initialize Apollo Client
+const client = new ApolloClient({
+  uri: 'https://countries.trevorblades.com/graphql',
+  cache: new InMemoryCache()
+});
+
 export default function App() {
   return (
-    <Provider store={store}>
-      <SafeAreaView style={{flex:1}}>
-        <NavigationContainer>
-              <Tab.Navigator initialRouteName='Main'
-                  screenOptions={({route}) => ({
-                      tabBarIcon: ({focused, color}) => {
-                        return ( 
-                          <View>
-                            {
-                              route.name == 'UseState' ?
-                              <Basketball/>
-                              : (route.name == 'UseEffect') ?
-                              <Bus/>
-                              : (route.name == 'UseMemo') ?
-                              <Credit/>
-                              : (route.name == 'ScreenRedux') ?
-                              <Iphone/>
-                              : null
-                            }
-                          </View>
-                        )
-                      },
-                  })}
-              >
-                  <Tab.Screen name='UseState'
-                      children={() => <UseState/>}
-                  />
-                  <Tab.Screen name='UseEffect'
-                      children={() => <UseEffect/>}
-                  />
-                  <Tab.Screen name='UseMemo'
-                      children={() => <UseMemo/>}
-                  />
-                  <Tab.Screen name='ScreenRedux'
-                      children={() => <ScreenRedux/>}
-                  />
-              </Tab.Navigator> 
-          </NavigationContainer>
-      </SafeAreaView>
-    </Provider>
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <SafeAreaView style={{flex:1}}>
+          <NavigationContainer>
+                <Tab.Navigator initialRouteName='Main'
+                    screenOptions={({route}) => ({
+                        tabBarIcon: ({focused, color}) => {
+                          return ( 
+                            <View>
+                              {
+                                route.name == 'UseState' ?
+                                <Basketball/>
+                                : (route.name == 'UseEffect') ?
+                                <Bus/>
+                                : (route.name == 'UseMemo') ?
+                                <Credit/>
+                                : (route.name == 'ScreenRedux') ?
+                                <Iphone/>
+                                : (route.name == 'GraphQL') ?
+                                <Car/>
+                                : null
+                              }
+                            </View>
+                          )
+                        },
+                    })}
+                >
+                    <Tab.Screen name='UseState'
+                        children={() => <UseState/>}
+                    />
+                    <Tab.Screen name='UseEffect'
+                        children={() => <UseEffect/>}
+                    />
+                    <Tab.Screen name='UseMemo'
+                        children={() => <UseMemo/>}
+                    />
+                    <Tab.Screen name='ScreenRedux'
+                        children={() => <ScreenRedux/>}
+                    />
+                    <Tab.Screen name='GraphQL'
+                        children={() => <GraphQL/>}
+                    />
+                </Tab.Navigator> 
+            </NavigationContainer>
+        </SafeAreaView>
+      </Provider>
+    </ApolloProvider>
   );
 }
 
---
+++ suslonovNikita/GraphQL/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { View, Pressable, Text, FlatList, ScrollView } from 'react-native'
+import { useQuery } from "@apollo/client";
+import { CONTINENT_QUERY } from "./Query";
+
+const GraphQl = () => {
+    const { data, loading } = useQuery(CONTINENT_QUERY);
+  
+    const ContinentItem = ({ countries }) => {
+      const { code } = countries;
+      const { name } = countries;
+  
+      return (
+        <Pressable style={{justifyContent:"center", alignItems:"center", height: 50, marginBottom: 30, backgroundColor: '#ffffff', borderRadius: 10}}>
+          <Text>{name}</Text>
+        </Pressable>
+      );
+    };
+  
+    if (loading) {
+      return <Text>Fetching data...</Text>
+    }
+  
+    return (
+        <FlatList
+            data={data.countries}
+            contentContainerStyle={{padding:30}}
+            renderItem={({ item }) => <ContinentItem countries={item} />}
+            keyExtractor={(item, index) => index}
+        />
+    );
+}
+
+export default GraphQl;
