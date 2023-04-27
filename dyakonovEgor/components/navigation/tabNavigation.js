c2421a57c3d95c3e2a404228683d643269ff8b88
egor dyakonov <egordyakonov2000@gmail.com>
2023-04-27 19:41:43 +0900

    redux

--- dyakonovEgor/components/navigation/tabNavigation.js
+++ dyakonovEgor/components/navigation/tabNavigation.js
@@ -1,14 +1,55 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-// import Icon from "react-native-vector-icons/FontAwesome";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import Lab2 from "../screens/lab2";
 import Lab3 from "../screens/lab3";
+import Lab4 from "../screens/lab4"
 
 const Tab = createBottomTabNavigator();
 const TabNavigator = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Lab2" component={Lab2} />
-      <Tab.Screen name="Lab3" component={Lab3} />
+      <Tab.Screen
+        name="Lab2"
+        component={Lab2}
+        options={{
+          tabBarLabel: "LAB 1",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="checkbox-multiple-blank-circle"
+              color={color}
+              size={26}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Lab3"
+        component={Lab3}
+        options={{
+          tabBarLabel: "LAB 3",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="image-multiple"
+              color={color}
+              size={26}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Lab4"
+        component={Lab4}
+        options={{
+          tabBarLabel: "LAB 4",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="image-multiple"
+              color={color}
+              size={26}
+            />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
---
+++ dyakonovEgor/components/screens/lab4.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./actions";
+
+function Counter() {
+  const count = useSelector((state) => state.count);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <p>{count}</p>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+    </div>
+  );
+}
+
+export default Counter;
