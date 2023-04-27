b0c20865156766649e70f850e6134f718018fb49
Second-Sail <nik200211@gmail.com>
2023-04-27 20:42:27 +0900

    added UseEffect and Navigation

--- suslonovNikita/App.js
+++ suslonovNikita/App.js
@@ -1,14 +1,79 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { Provider } from 'react-redux';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+import store from './Redux'
 import UseState from './UseState';
+import UseEffect from './UseEffect';
+import UseMemo from './UseMemo';
+import ScreenRedux from './ScreenRedux';
+import GraphQL from './GraphQL';
+
+import Bus from './assets/svg/bus';
+import Basketball from './assets/svg/basketball';
+import Credit from './assets/svg/credit';
+import Iphone from './assets/svg/iphone';
+import Car from './assets/svg/car';
+
+const Tab = createBottomTabNavigator();
+// Initialize Apollo Client
+const client = new ApolloClient({
+  uri: 'https://countries.trevorblades.com/graphql',
+  cache: new InMemoryCache()
+});
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Suslonov</Text>
-      <StatusBar style="auto" />
-      <UseState/>
-    </View>
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <SafeAreaView style={{flex:1}}>
+          <NavigationContainer>
+                <Tab.Navigator initialRouteName='Main'
+                    screenOptions={({route}) => ({headerStyle: {backgroundColor:"#3B82F6"}, tabBarStyle: {backgroundColor: "#FFFFFF"},
+                        tabBarIcon: ({focused, color}) => {
+                          return ( 
+                            <View>
+                              {
+                                route.name == 'UseState' ?
+                                <Basketball/>
+                                : (route.name == 'UseEffect') ?
+                                <Bus/>
+                                : (route.name == 'UseMemo') ?
+                                <Credit/>
+                                : (route.name == 'ScreenRedux') ?
+                                <Iphone/>
+                                : (route.name == 'GraphQL') ?
+                                <Car/>
+                                : null
+                              }
+                            </View>
+                          )
+                        },
+                    })}
+                >
+                    <Tab.Screen name='UseState'
+                        children={() => <UseState/>}
+                    />
+                    <Tab.Screen name='UseEffect'
+                        children={() => <UseEffect/>}
+                    />
+                    <Tab.Screen name='UseMemo'
+                        children={() => <UseMemo/>}
+                    />
+                    <Tab.Screen name='ScreenRedux'
+                        children={() => <ScreenRedux/>}
+                    />
+                    <Tab.Screen name='GraphQL'
+                        children={() => <GraphQL/>}
+                    />
+                </Tab.Navigator> 
+            </NavigationContainer>
+        </SafeAreaView>
+      </Provider>
+    </ApolloProvider>
   );
 }
 
