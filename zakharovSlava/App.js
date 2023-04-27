23e04a4b51aa451a000bc902a7c407b6f8a44674
Zokhar <benderasd2001@mail.ru>
2023-04-27 20:01:36 +0900

    prettier

--- zakharovSlava/App.js
+++ zakharovSlava/App.js
@@ -1,11 +1,25 @@
 import { NavigationContainer } from "@react-navigation/native";
 import TabNavigation from "./components/navigations/Navigation";
 
+import { Provider } from 'react-redux';
+import store from './components/redux'
+
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: "https://rickandmortyapi.com/graphql/",
+  cache: new InMemoryCache()
+});
+
 const App = () => {
   return (
-    <NavigationContainer>
-      <TabNavigation />
-    </NavigationContainer>
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <NavigationContainer>
+          <TabNavigation />
+        </NavigationContainer>
+      </Provider>
+    </ApolloProvider>
   );
 };
 
--- zakharovSlava/components/screens/Task2.js
+++ zakharovSlava/components/screens/Task2.js
@@ -1,30 +1,36 @@
 import { useState, useEffect } from "react";
-import { SafeAreaView, Text, TouchableOpacity, View, StyleSheet, Button } from "react-native";
+import {
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  View,
+  StyleSheet,
+  Button,
+} from "react-native";
 
 const Task2 = () => {
-
-    const [count, setCount] = useState(0);
-    useEffect(() => {
-        setTimeout(() => {
-            setCount((count) => count + 1);
-        }, 1000);
-    });
-    return (
-        <SafeAreaView
-            style={{
-                backgroundColor: 'white',
-                flex: 1,
-                justifyContent: "center",
-            }}
-        >
-            <View style={{ margin: 20 }}>
-                <View style={{ alignItems: "center" }}>
-                    <Text style={{ color: 'black', fontSize: 20, fontWeight: "800" }}>
-                        {count}
-                    </Text>
-                </View>
-            </View>
-        </SafeAreaView>
-    )
-}
-export default Task2
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    setTimeout(() => {
+      setCount((count) => count + 1);
+    }, 1000);
+  });
+  return (
+    <SafeAreaView
+      style={{
+        backgroundColor: "white",
+        flex: 1,
+        justifyContent: "center",
+      }}
+    >
+      <View style={{ margin: 20 }}>
+        <View style={{ alignItems: "center" }}>
+          <Text style={{ color: "black", fontSize: 20, fontWeight: "800" }}>
+            {count}
+          </Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+export default Task2;
