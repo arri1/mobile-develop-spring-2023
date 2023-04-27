65e5fff51e6b74c6a7f4d7810dc892026a67d709
Nyursultan <nyurguntarskii@gmail.com>
2023-04-27 20:27:02 +0900

    Update Lab5.js

--- tarskyNyurgun/src/labs/Lab5.js
+++ tarskyNyurgun/src/labs/Lab5.js
@@ -3,7 +3,7 @@ import { ActivityIndicator, Text, View, StyleSheet } from "react-native";
 import { gql, useQuery } from "@apollo/client";
 
 const Lab5 = ({ navigation }) => {
-  const QUERY = gql`
+  const query = gql`
     query {
       characters(page: 1, filter: { name: "a" }) {
         results {
@@ -14,7 +14,7 @@ const Lab5 = ({ navigation }) => {
     }
   `;
 
-  const { data, loading } = useQuery(QUERY);
+  const { data, loading } = useQuery(query);
   console.log(data);
   if (loading)
     return (
