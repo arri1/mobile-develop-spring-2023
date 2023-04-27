f83e4ca4c89d471bb5f28f122fc7f3a6c30975fe
serpentim <54822621+serpentim@users.noreply.github.com>
2023-04-27 20:22:36 +0900

    Lab5

--- ivanovTim/App.js
+++ ivanovTim/App.js
@@ -1,17 +1,17 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
-import { Provider } from 'react-redux';
-import store from './store/store';
+import * as React from "react";
+import { Text, View } from "react-native";
+import { Provider } from "react-redux";
+import store from "./store/store";
 import { ApolloProvider } from "@apollo/client";
 import { client } from "./apollo";
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import Lab1 from './Labs/Lab1';
-import Lab2 from './Labs/Lab2';
-import Lab3 from './Labs/Lab3';
-import Lab4 from './Labs/Lab4';
-import Lab5 from './Labs/Lab5';
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import Lab1 from "./Labs/Lab1";
+import Lab2 from "./Labs/Lab2";
+import Lab3 from "./Labs/Lab3";
+import Lab4 from "./Labs/Lab4";
+import Lab5 from "./Labs/Lab5";
 
 const Tab = createBottomTabNavigator();
 
@@ -21,26 +21,33 @@ const App = () => {
       <Provider store={store}>
         <NavigationContainer>
           <Tab.Navigator
-            screenOptions={({route}) => ({
-              tabBarIcon: ({focused, size, color}) => {
-                if (route.name == 'Lab1') {
-                  iconName = focused ? 'numeric-1-box' : 'numeric-1';
+            screenOptions={({ route }) => ({
+              tabBarIcon: ({ focused, size, color }) => {
+                if (route.name == "Lab1") {
+                  iconName = focused ? "numeric-1-box" : "numeric-1";
                 }
-                if (route.name == 'Lab2') {
-                  iconName = focused ? 'numeric-2-box' : 'numeric-2';
+                if (route.name == "Lab2") {
+                  iconName = focused ? "numeric-2-box" : "numeric-2";
                 }
-                if (route.name == 'Lab3') {
-                  iconName = focused ? 'numeric-3-box' : 'numeric-3';
+                if (route.name == "Lab3") {
+                  iconName = focused ? "numeric-3-box" : "numeric-3";
                 }
-                if (route.name == 'Lab4') {
-                  iconName = focused ? 'numeric-4-box' : 'numeric-4';
+                if (route.name == "Lab4") {
+                  iconName = focused ? "numeric-4-box" : "numeric-4";
                 }
-                if (route.name == 'Lab5') {
-                  iconName = focused ? 'numeric-5-box' : 'numeric-5';
+                if (route.name == "Lab5") {
+                  iconName = focused ? "numeric-5-box" : "numeric-5";
                 }
-                return <MaterialCommunityIcons name={iconName} size={size} color={color} />;;
-              }
-            })}>
+                return (
+                  <MaterialCommunityIcons
+                    name={iconName}
+                    size={size}
+                    color={color}
+                  />
+                );
+              },
+            })}
+          >
             <Tab.Screen name="Lab1" component={Lab1} />
             <Tab.Screen name="Lab2" component={Lab2} />
             <Tab.Screen name="Lab3" component={Lab3} />
@@ -51,6 +58,6 @@ const App = () => {
       </Provider>
     </ApolloProvider>
   );
-}
+};
 
 export default App;
