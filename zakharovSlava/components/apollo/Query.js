6fd3a823a1675a2886e56badc797ac74c748555e
Zokhar <benderasd2001@mail.ru>
2023-04-27 20:22:57 +0900

    add apollo

---
+++ zakharovSlava/components/apollo/Query.js
@@ -0,0 +1,15 @@
+import { gql } from "@apollo/client";
+
+export const CHARACTERS_QUERY = gql`
+  query GetCharacters {
+    characters {
+      results {
+        name
+        status
+        species
+        gender
+        image
+      }
+    }
+  }
+`;
