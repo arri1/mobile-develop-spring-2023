4382e097fc0d23d47bfb4d30b9f7f6053205b19f
Милана Иванова <ivanova544252@mail.ru>
2023-04-27 22:47:34 +0900

    redux counter

--- ivanovaMilana/components/navigations/tabNavigation.js
+++ ivanovaMilana/components/navigations/tabNavigation.js
@@ -2,15 +2,20 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Lab1 from "../screens/lab1";
 import Lab2 from "../screens/lab2";
 import Lab3 from "../screens/lab3";
+import Lab5 from "../screens/lab5";
+import { Provider } from "react-redux";
+import { store } from '../redux/store';
 
 const Tab = createBottomTabNavigator();
 const TabNavigation = () => {
     return (
-    <Tab.Navigator>
-        <Tab.Screen name="Lab1" component={Lab1} />
-        <Tab.Screen name="Lab2" component={Lab2} />
-        <Tab.Screen name="Lab3" component={Lab3} />
-    </Tab.Navigator>
+        
+            <Tab.Navigator>
+                <Tab.Screen name="Lab1" component={Lab1} />
+                <Tab.Screen name="Lab2" component={Lab2} />
+                <Tab.Screen name="Lab3" component={Lab3} />
+                <Tab.Screen name="Lab5" component={()=><Provider store={store}><Lab5 /></Provider>} />
+            </Tab.Navigator>
     );
 };
 
