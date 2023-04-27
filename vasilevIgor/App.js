7f559490e62a8691ebb328b54742ef7ca7d1e2be
overfrenzy <xplo1d387@gmail.com>
2023-04-27 20:30:57 +0900

    lab6 apollo client

--- vasilevIgor/App.js
+++ vasilevIgor/App.js
@@ -2,16 +2,23 @@ import TabNavigation from "./components/navigations/tabNavigation";
 import { NavigationContainer } from "@react-navigation/native";
 import { Provider } from "react-redux";
 import store from "./store";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import * as React from "react";
 
+const client = new ApolloClient({
+  uri: "https://flyby-router-demo.herokuapp.com/",
+  cache: new InMemoryCache(),
+});
 
 const App = () => {
   return (
+    <ApolloProvider client={client}>
       <Provider store={store}>
         <NavigationContainer>
           <TabNavigation />
         </NavigationContainer>
       </Provider>
+    </ApolloProvider>
   );
 };
 
