8843b3b6730cb33d20dc02c3b7a3c4e7a8afb70d
danildiodorov <ddldiodorov@gmail.com>
2023-05-29 20:32:40 +0900

    lab1

--- diodorovDanil/App.js
+++ diodorovDanil/App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { useState } from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [state, setState] = useState(0)
+    return (
+        <View style={styles.container}>
+            <Text>{state}</Text>
+            <TouchableOpacity
+                style={styles.button}
+                onPress={() => setState(state + 1)}
+            >
+                <Text>+1</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={styles.button}
+                onPress={() => setState(state - 1)}
+            >
+                <Text>-1</Text>
+            </TouchableOpacity>
+            <StatusBar style="auto" />
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    button: {
+        alignItems: 'center',
+        backgroundColor: '#DDDDDD',
+        padding: 10,
+        width: 200,
+        marginTop: 10,
+    },
+})
