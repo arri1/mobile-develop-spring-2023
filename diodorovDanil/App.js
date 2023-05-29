4c040a8b6ff543a19542e92c90d262dcf516d234
danildiodorov <ddldiodorov@gmail.com>
2023-05-29 20:54:25 +0900

    lab2

--- diodorovDanil/App.js
+++ diodorovDanil/App.js
@@ -1,41 +1,75 @@
-import { StatusBar } from 'expo-status-bar'
-import { useState } from 'react'
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { StyleSheet } from 'react-native'
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
+import { COLORS } from './config'
+import Task1 from './pages/Task1'
+import Icon1 from 'react-native-vector-icons/AntDesign'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Task2 from './pages/Task2'
 
-export default function App() {
-    const [state, setState] = useState(0)
+const Tab = createBottomTabNavigator()
+
+const MyTheme = {
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        background: COLORS.PRIMARY,
+    },
+}
+
+const App = () => {
     return (
-        <View style={styles.container}>
-            <Text>{state}</Text>
-            <TouchableOpacity
-                style={styles.button}
-                onPress={() => setState(state + 1)}
+        <NavigationContainer theme={MyTheme}>
+            <Tab.Navigator
+                screenOptions={({ route }) => ({
+                    tabBarIcon: ({ focused }) => {
+                        let icon
+                        let color = focused ? 'white' : COLORS.BLUE
+                        if (route.name === 'Task1') {
+                            icon = (
+                                <Icon1
+                                    name="filetext1"
+                                    size={30}
+                                    color={color}
+                                />
+                            )
+                        } else if (route.name === 'Task2') {
+                            icon = (
+                                <Icon1
+                                    name="filetext1"
+                                    size={30}
+                                    color={color}
+                                />
+                            )
+                        }
+                        return icon
+                    },
+                    tabBarActiveTintColor: 'tomato',
+                    tabBarInactiveTintColor: 'gray',
+                    tabBarLabel: () => {
+                        return null
+                    },
+                    tabBarStyle: {
+                        backgroundColor: COLORS.SECONDARY,
+                        height: 90,
+                        borderTopWidth: 0,
+                    },
+                })}
             >
-                <Text>+1</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-                style={styles.button}
-                onPress={() => setState(state - 1)}
-            >
-                <Text>-1</Text>
-            </TouchableOpacity>
-            <StatusBar style="auto" />
-        </View>
+                <Tab.Screen
+                    name="Task1"
+                    component={Task1}
+                    options={{ headerShown: false }}
+                />
+                <Tab.Screen
+                    name="Task2"
+                    component={Task2}
+                    options={{ headerShown: false }}
+                />
+            </Tab.Navigator>
+        </NavigationContainer>
     )
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    button: {
-        alignItems: 'center',
-        backgroundColor: '#DDDDDD',
-        padding: 10,
-        width: 200,
-        marginTop: 10,
-    },
-})
+const styles = StyleSheet.create({})
+
+export default App
